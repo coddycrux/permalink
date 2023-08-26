cc64d098cc4a6f3ff57d0c7bb7a1ea2ed42abf5b
coddy <69899814+coddycrux@users.noreply.github.com>
2023-08-26 21:52:49 +0300

    Update ms-dest.bat

--- ms-dest.bat
+++ ms-dest.bat
@@ -1,5 +1,13 @@
+@REM The MS-DEST (Microsft DHCP Discovery Engine Setup Tool) takes care of most of the setup work
+@REM regarding setting up the copy task into the Gateway server.
+@REM It generates RSA keys, sets up the environment, transfers keys to GW, and schedules the task
+@REM Author: roycoh@checkpoint.com
+
 @echo off
+@REM Note that all variables we set here are local, they stop existing after the corresponding 'endlocal'
 setlocal
+@REM Make sure we're in the C: drive. Important when script is opened from a flash drive / CD
+c:
 
 echo CheckPoint Technologies Ltd. - Quantum IoT
 echo MS-DHCP Discovery Engine Setup Tool
@@ -16,16 +24,12 @@ if %errorLevel% NEQ 0 (
     echo Please make sure to right click the file ^> "Run as Administrator".
     goto exit
 )
-echo -- Setup run as Administrator.
 echo(
 
 @REM Check if SSH is installed
 @REM where: prints location of command, and returns non-zero if not found
 where ssh >nul 2>&1
-if %errorLevel% == 0 (
-    echo -- SSH is installed.
-    goto modeSelect
-)
+if %errorLevel% == 0 goto modeSelect
 
 @REM Situation: SSH not installed
 echo SSH couldn't be found on this machine.
@@ -50,12 +54,13 @@ goto exit
 
 :installSSH
 @REM Situation: SSH not installed and client chose to install automatically
-@REM First of all, to instal SSH we need to run a powershell webrequest (wget).
+@REM First of all, to install SSH we need to run a powershell webrequest (wget).
 @REM Invoke-WebRequest exists only in PowerShell 3.0+, which by default comes with Windows Server 2012
 echo(
 
 :installSSH_checkPowershellExists
 @REM Make sure powershell is installed in the first place
+echo -- Making sure machine is compatible...
 where powershell >nul 2>&1
 if %errorLevel% NEQ 0 (
     echo PowerShell could not be found on this machine!
@@ -63,7 +68,6 @@ if %errorLevel% NEQ 0 (
     echo You may install PowerShell version 3.0 or higher, or install OpenSSH and re-run this tool.
     goto exit
 )
-echo -- PowerShell exists on machine
 
 :installSSH_checkWgetExists
 @REM Make sure Invoke-WebRequest exists (meaning powershell is version 3.0 or above)
@@ -73,7 +77,7 @@ if %errorLevel% NEQ 0 (
     echo Please install PowerShell version 3.0 or higher, and re-run the installer.
     goto exit
 )
-echo -- PowerShell version supported
+echo -- Auto-installer supported
 echo( 
 
 :installSSH_downloadInstalller
@@ -105,15 +109,14 @@ goto exit
 :installSSH_downloadSuccessful
 @REM Once the download is done, we need to run the .msi file.
 @REM It automatically installs SSH and adds it to the path.
-echo -- SSH download successful
+echo -- Download successful
 echo -- Running SSH installer...
 C:/Users/%USERNAME%/openSSHInstaller.msi
-echo -- OpenSSH installed
-echo -- Cleaning up installer
-del C:/Users/%USERNAME%/openSSHInstaller.msi 2>nul
-echo -- Installation complete!
+cd C:/Users/%USERNAME%
+del openSSHInstaller.msi 2>nul
+echo -- SSH installation complete
 echo(
-echo Please re-open this tool ^(in a new window^) to refresh.
+echo Please re-log into your system, and re-open this tool to refresh.
 goto exit
 
 
@@ -129,18 +132,24 @@ goto exit
 
 @REM Mode select between installing, uninstalling or changing IP
 :modeSelect
-@REM echo You can choose a mode to run, or quit.
-@REM echo ^(1^) Install Dicovery Engine
-@REM echo ^(3^) Update Management IP
-@REM echo ^(5^) Uninstall Discovery Engine
-@REM echo ^(0^) Close tool
-@REM echo(
+echo You can choose a mode to run, or quit.
+echo 1^) Install Dicovery Engine
+echo 2^) Update Gateway IP
+echo 3^) Uninstall Discovery Engine
+echo 4^) Close tool
+echo(
 
-@REM set /p "SelectedMode=Selected mode (default: 1): "
-@REM if /i "%SelectedMode%" == "0" goto instantExit
-@REM if /i "%SelectedMode%" == "1" goto installMode
+:modeSelect_prompt
+set /p "SelectedMode=Select a mode (1-4): "
+if /i "%SelectedMode%" == "1" goto installMode
+if /i "%SelectedMode%" == "2" goto updateIPMode
+if /i "%SelectedMode%" == "3" goto uninstallMode
+if /i "%SelectedMode%" == "4" goto instantExit
 
-
+@REM Situation: User picked a non-existent mode. Re-display mode select
+echo Please select a valid mode number ^(1-4^).
+echo(
+goto modeSelect_prompt
 
 
 
@@ -153,14 +162,12 @@ goto exit
 @REM                       Install mode
 @REM =======================================================
 
-@REM Enter install mode
 :installMode
-echo(
-echo -- Entering install mode
+echo Installing discovery engine
 echo(
 
 :instmode_enterIP
-set /p "IP=Enter Management server's IP: "
+set /p "IP=Enter Gateway server's IP: "
 echo Please make sure the IP is correct by navigating to
 echo https://%IP%/
 set /p "IPConfirm=Did the GAiA login page show up (y/n)? "
@@ -168,13 +175,13 @@ if /i "%IPConfirm%" == "y" goto instmode_enterIP_IPCorrect
 if /i "%IPConfirm%" == "Y" goto instmode_enterIP_IPCorrect
 
 :instmode_enterIP_IPWrong
-echo Make sure the Management server has been properly set up, or try inputting the
+echo Make sure the Gateway server is up and configured, or try inputting the
 echo correct IP address.
 goto instmode_enterIP
 
 :instmode_enterIP_IPCorrect
-echo -- Setting up discovery engine for server %IP%
 echo(
+echo -- Setting up discovery engine for server %IP%
 
 :instmode_generateRSA
 echo -- Generating RSA key pair...
@@ -182,74 +189,140 @@ cd C:/Users/%USERNAME%/
 mkdir .cp-ssh 2>nul
 cd .cp-ssh
 del id_rsa id_rsa.pub 2>nul
-ssh-keygen -N "" -q -f id_rsa
-echo -- Key pair generated.
-echo(
+ssh-keygen -t rsa -b 3072 -N "" -q -f id_rsa
 
 @REM Generate a "Read me!" file
 :instmode_generateReadme
+echo -- Generating readme...
 cd C:/Users/%USERNAME%/.cp-ssh
 echo Do not delete this folder! > "Read me first!.txt"
 echo It's necessary for Checkpoint's MS-DHCP discovery engine to properly function. >> "Read me first!.txt"
 echo( >> "Read me first!.txt"
-echo If you want to properly uninstall the MS-DHCP discovery engine, use the supplied uninstaller. >> "Read me first!.txt"
+echo If you want to properly uninstall the MS-DHCP discovery engine, use the supplied uninstaller >> "Read me first!.txt"
 echo for doing so safely. >> "Read me first!.txt"
 
 @REM This step may be removed if the setup script is added to the MS-DHCP agent
-:instmode_prepareMGMT
-echo -- Preparing Management environment for password-less SSH
-echo Please enter your Management server's password. Don't worry when not seeing as you type.
+:instmode_prepareGW
+echo -- Preparing Gateway environment for password-less SSH
+echo(
+echo Please enter your Gateway server's password. Don't worry when not seeing as you type.
 echo If you've made a mistake, press backspace sufficiently and retry.
-:instmode_prepareMGMT_runCmd
-ssh -q -o StrictHostKeyChecking=no admin@%IP% "mkdir -p /var/log/nanoagent/ms-dhcp-logs && cd /home/admin && mkdir -p .ssh && chmod 700 .ssh && cd .ssh && touch authorized_keys && chmod 644 authorized_keys && sed -i 's/^[ ]*\#\?[ ]*PubkeyAuthentication.*/PubkeyAuthentication yes/g' /etc/ssh/sshd_config && sed -i 's/^[ ]*\#\?[ ]*PasswordAuthentication.*/PasswordAuthentication no/g' /etc/ssh/sshd_config && sed -i 's/^[ ]*\#\?[ ]*AuthorizedKeysFile.*/AuthorizedKeysFile \/home\/admin\/.ssh\/authorized_keys/g' /etc/ssh/sshd_config && /etc/init.d/sshd restart"
+:instmode_prepareGW_runCmd
+ssh -q -o StrictHostKeyChecking=no admin@%IP% "mkdir -p /var/log/iot-discovery/ms-dhcp-logs && cd /home/admin && mkdir -p .ssh && chmod 700 .ssh && cd .ssh && touch authorized_keys && chmod 644 authorized_keys && sed -i 's/^[ ]*\#\?[ ]*PubkeyAuthentication.*/PubkeyAuthentication yes/g' /etc/ssh/sshd_config && sed -i 's/^[ ]*\#\?[ ]*PasswordAuthentication.*/PasswordAuthentication no/g' /etc/ssh/sshd_config && sed -i 's/^[ ]*\#\?[ ]*AuthorizedKeysFile.*/AuthorizedKeysFile \/home\/admin\/.ssh\/authorized_keys/g' /etc/ssh/sshd_config && sed -i 's/^[ ]*\#\?[ ]*StrictModes.*/StrictModes yes/g' /etc/ssh/sshd_config && /etc/init.d/sshd restart" >nul
 if %errorLevel% NEQ 0 (
     echo Authentication failed. Please make sure you type the correct password.
     echo If it helps, you can try typing the password somewhere, copying it
     echo and pasting into the prompt ^(by right-clicking^).
-    goto instmode_prepareMGMT_runCmd
+    goto instmode_prepareGW_runCmd
 )
-echo -- Management environment set up.
 echo(
 
 :instmode_copyPublicKey
-echo -- Copying public key into the Management server
+echo -- Copying public key into the Gateway server
+echo(
 echo Please enter your password again.
 :instmode_copyPublicKey_runCmd
-scp -q -o StrictHostKeyChecking=no C:/Users/%USERNAME%/.cp-ssh/id_rsa.pub admin@%IP%:/home/admin/.ssh/authorized_keys
+set /p PubKey=<id_rsa.pub
+ssh -q -o StrictHostKeyChecking=no admin@%IP% "printf '\n%%s\n' '%PubKey%' >> /home/admin/.ssh/authorized_keys"
 if %errorLevel% NEQ 0 (
     echo Authentication failed. Please make sure you type the correct password.
     echo If it helps, you can try typing the password somewhere, copying it
     echo and pasting into the prompt ^(by right-clicking^).
     goto instmode_copyPublicKey_runCmd
 )
-echo -- Public key copied
 echo(
 
 :instmode_changeRSAPerms
 echo -- Setting permissions of private key to SYSTEM only
 icacls C:/Users/%USERNAME%/.cp-ssh/id_rsa /setowner SYSTEM >nul
 icacls C:/Users/%USERNAME%/.cp-ssh/id_rsa /remove %USERNAME% >nul
-echo -- Permissions set
-echo(
 
-:instmode_createCopyJob
-echo -- Creating copy job to upload logs into Management server
-echo(
-:instmode_createCopyJob_genBatch
-echo -- Generating batch file with copy command
-echo scp -i C:/Users/%USERNAME%/.cp-ssh/id_rsa -o StrictHostKeyChecking=no -r "C:/Windows/System32/dhcp/*DhcpSrvLog*.log" admin@%IP%:/var/log/nanoagent/ms-dhcp-logs > C:/Users/%USERNAME%/.cp-ssh/copy-job.bat
-echo -- Batch generated
-echo(
-:instmode_createCopyJob_scheduleTask
-echo -- Scheduling task with administrator privileges to copy every 1 minute
-schtasks /create /tn "Checkpoint DHCP Discovery" /sc MINUTE /mo 1 /tr "C:/Users/%USERNAME%/.cp-ssh/copy-job.bat" /ru System /rl HIGHEST >nul
-echo -- Task scheduling done
+:instmode_scheduleTask
+echo -- Scheduling task to copy logs every 1 minute
+@REM First uninstall the task if it already exists
+schtasks /end /tn "Checkpoint DHCP Discovery" >nul 2>&1
+schtasks /delete /tn "Checkpoint DHCP Discovery" /f >nul 2>&1
+@REM Then create a new task
+schtasks /create /tn "Checkpoint DHCP Discovery" /sc MINUTE /mo 1 /tr "scp -i C:/Users/%USERNAME%/.cp-ssh/id_rsa -o StrictHostKeyChecking=no -r 'C:/Windows/System32/dhcp/*DhcpSrvLog-*.log' admin@%IP%:/var/log/iot-discovery/ms-dhcp-logs/" /ru System /rl HIGHEST >nul
 echo(
 
 :instmode_successful
 echo Discovery engine setup successful.
-echo Make sure to select MS-DHCP as your discovery engine in Infinity Portal.
+echo Make sure to select MS-DHCP in your Quantum IoT Profile ^(in Infinity Portal^), and enforce.
+echo It's crucial to enforce the profile as soon as possible.
 echo(
+goto exit
+
+
+
+@REM =======================================================
+@REM                     Update IP mode
+@REM =======================================================
+
+:updateIPMode
+echo Updating Gateway IP
+echo(
+
+:updtmode_enterIP
+set /p "IP=Enter new Gateway server's IP: "
+echo Please make sure the IP is correct by navigating to
+echo https://%IP%/
+set /p "IPConfirm=Did the GAiA login page show up (y/n)? "
+if /i "%IPConfirm%" == "y" goto updtmode_enterIP_IPCorrect
+if /i "%IPConfirm%" == "Y" goto updtmode_enterIP_IPCorrect
+
+:instmode_enterIP_IPWrong
+echo Please make sure the IP is correct.
+goto instmode_enterIP
+
+
+:updtmode_enterIP_IPCorrect
+echo(
+echo -- Removing scheduled copy task
+schtasks /end /tn "Checkpoint DHCP Discovery" >nul 2>&1
+schtasks /delete /tn "Checkpoint DHCP Discovery" /f >nul 2>&1
+
+echo -- Creating new task
+schtasks /create /tn "Checkpoint DHCP Discovery" /sc MINUTE /mo 1 /tr "scp -i C:/Users/%USERNAME%/.cp-ssh/id_rsa -o StrictHostKeyChecking=no -r 'C:/Windows/System32/dhcp/*DhcpSrvLog-*.log' admin@%IP%:/var/log/iot-discovery/ms-dhcp-logs/" /ru System /rl HIGHEST >nul
+
+echo(
+echo IP updated successfully!
+echo(
+goto exit
+
+@REM =======================================================
+@REM                     Uninstall mode
+@REM =======================================================
+
+:uninstallMode
+
+set /p "ShouldUninstall=Are you sure you want to uninstall the dicovery engine (y/n)? "
+if /i "%ShouldUninstall%" == "y" goto uninmode_shouldUninstall
+if /i "%ShouldUninstall%" == "Y" goto uninmode_shouldUninstall
+
+@REM Situation: User doesn't want to uninstall. Return to mode select
+echo(
+goto modeSelect
+
+:uninmode_shouldUninstall
+echo Uninstalling discovery engine
+echo(
+
+:uninmode_removeTask
+echo -- Removing scheduled copy task
+schtasks /end /tn "Checkpoint DHCP Discovery" >nul 2>&1
+schtasks /delete /tn "Checkpoint DHCP Discovery" /f >nul 2>&1
+
+:uninmode_deleteCpSSH
+echo -- Deleting checkpoint folder
+cd C:/Users/%USERNAME%/
+rmdir /S /Q .cp-ssh >nul 2>&1
+
+:uninmode_successful
+echo(
+echo Dicovery engine successfully uninstalled.
+echo(
+goto exit
 
 :exit
 endlocal
